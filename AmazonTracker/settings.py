19f88b28f15b51cbe4410ecece3415d7b094a908
nasir733 <60297068+nasir733@users.noreply.github.com>
2020-07-20 21:15:05 +0500

    trying to deploy on heroku

--- AmazonTracker/settings.py
+++ AmazonTracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -114,9 +115,17 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+django_heroku.settings(locals())
--- tracker/amazon_config.py
+++ tracker/amazon_config.py
@@ -1,8 +1,8 @@
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
 from django.shortcuts import render,HttpResponse
 from django.views import View
-
+import os
 DIRECTORY = 'tracker/reports'
 
 
@@ -10,16 +10,16 @@ BASE_URL = "http://www.amazon.de/"
 
 
 def get_chrome_web_driver(options):
-    return webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+    return webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
 
 
 def get_web_driver_options():
-    return webdriver.FirefoxOptions()
+    return webdriver.ChromeOptions()
 
 
 def set_ignore_certificate_error(options):
     options.add_argument('--ignore-certificate-errors')
-
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 def set_browser_as_incognito(options):
     options.add_argument('--incognito')
@@ -27,3 +27,6 @@ def set_browser_as_incognito(options):
 
 def set_automation_as_head_less(options):
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-sh-usage')
+    
